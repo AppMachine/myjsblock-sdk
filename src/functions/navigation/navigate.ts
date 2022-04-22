8281b97f02e42e6e80e297ac2e309286b854f5b5
Eric Fennis <eric@dreamteam.nl>
2022-04-22 12:06:40 +0200

    remove word "building"

--- src/functions/navigation/navigate.ts
+++ src/functions/navigation/navigate.ts
@@ -3,15 +3,15 @@ import MyJsBlockFunction from '../../types/function'
 
 enum GoToBlockErrors {
   UNKNOWN_ERROR = 'UNKNOWN_ERROR',
-  BLOCK_ID_NOT_FOUND = 'BLOCK_ID_NOT_FOUND'
+  VARIABLE_NAME_NOT_FOUND = 'VARIABLE_NAME_NOT_FOUND'
 }
 
 export type NavigateProperties = Record<string, unknown>
 
 /**
- * Will navigate to a building block matching the given block id.
+ * Will navigate to a block matching the given block id.
  * @public
- * @param   {String}    variableName The identifier of the building block, this can be the caption, variable name or id.
+ * @param   {String}    variableName The identifier of the block, this can be the caption, variable name or id.
  * @throws  {Error}     If the block identifier is not of type string or is undefined.
  */
 const navigate = (variableName: string, properties: NavigateProperties): Promise<void> =>
