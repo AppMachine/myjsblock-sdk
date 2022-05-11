bb0478f8e1ef6d3d8c09dde889eff2e31dfe40fd
Eric Fennis <eric.fennis@gmail.com>
2022-05-11 15:57:28 +0200

    Add Example (#9)
    * Add example
    
    * Fix selector

---
+++ examples/show-loader-button/main.js
@@ -0,0 +1,13 @@
+import './style.css'
+import { showLoader, hideLoader } from '@myjsblock/sdk'
+
+const loadingButton = document.getElementById('loader-button')
+
+const onClick = async () => {
+  await showLoader()
+
+  await new Promise((resolve) => { setTimeout(resolve, 2000) })
+  await hideLoader()
+}
+
+loadingButton.addEventListener('click', onClick)
